1fca10c44edf2304cd8df49929209e3a834b2c4b
hongcheng.ghc <hongcheng.ghc@digital-engine.com>
2023-01-19 19:25:39 +0800

    fix: add typeId

--- src/model/account.js
+++ src/model/account.js
@@ -10,6 +10,10 @@ module.exports = ({ sequelize }) => {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    typeId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
     date: {
       type: DataTypes.DATEONLY,
       allowNull: false,
